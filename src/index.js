4d7f0a81f5ee9e19b706920a1e2f3176242d0995
= <=>
2024-08-24 19:20:55 +0530

    extracted card component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
+import emojipedia from "./emojipedia";
 
+const arr = emojipedia.map((x) => x.meaning.substring(0, 100));
+console.log(arr);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
